591df6dfd61259a0a482680fe0c95d91513b9757
ctf005 <chris.falch@amfam.com>
2014-10-20 22:36:16 -0500

    Gave up temporarily on fetching the Wunderlist. Instead, mocked a test-data file with a couple books. Use this to pull Amz-data for display.

--- js/readinglist.js
+++ js/readinglist.js
@@ -8,4 +8,35 @@
 		};
 	})
 
+	/**
+	 * The controler for  getting the book list, and then getting the
+	 * data for each book. It should delegate to factories for the actual calls.
+	 */
+	app.controller('ListController', function($http) {
+		var params = {
+			email: 'phate08@gmail.com',
+			password: 'wunderful'
+		},
+		httpObj = {
+            method: 'jsonp',
+            url: 'http://api.wunderlist.com',
+            params: weatherSearchObj
+        };
+	});
+
+
+	/**
+	 * The factory for getting the book list from Wunderlist.
+	 */
+	app.factory('ListFetcher', function($http) {
+
+	});
+	
+	/**
+	 * The fatory for getting the book data from Amazon.
+	 */
+	app.factory('BookFetcher', function($http) { 
+
+	});
+
 })();
